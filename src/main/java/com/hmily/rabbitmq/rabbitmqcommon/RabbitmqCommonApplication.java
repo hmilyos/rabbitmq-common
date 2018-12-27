9f903e9fcad7fa9dbbdb8c3438eff2841ac857c0
欧世明（深圳联友科技有限公司IS事业部项目一部制造领域) <ly-oushim@dfl.com.cn>
2018-12-27 08:11:33 +0800

    init

---
+++ src/main/java/com/hmily/rabbitmq/rabbitmqcommon/RabbitmqCommonApplication.java
@@ -0,0 +1,14 @@
+package com.hmily.rabbitmq.rabbitmqcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqCommonApplication.class, args);
+	}
+
+}
+
